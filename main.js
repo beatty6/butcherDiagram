931fd0f53604113a2cf32f4c09127f7991e54fa0
beatty6 <beatty6@pdx.edu>
2020-05-16 10:30:45 -0700

    photos upload

--- main.js
+++ main.js
@@ -1,53 +1,11 @@
 $(document).ready(function () {
-    $("g").addClass("red-parts");
 
-
-    $("path").addClass("path");
-    //    $(".path").css({fill:"brown",opacity:.5});
-
-    $(".red-parts").mouseover(function () {
-        $(this).css({
-            opacity: 1
-        });
-        $(this).find(".path").css({
-            opacity: 1,
-            fill: "#ffffff",
-            stroke: "#FFC0CB",
-            strokeWidth: "3"
-        });
-
-        anime({
-            targets: '.red-parts path',
-            strokeDashoffset: [anime.setDashoffset, 0],
-            easing: 'easeInOutSine',
-            duration: 800,
-            direction: 'alternate',
-            loop: false
+    $("div.cut").mouseover(function () {
+        $(this).children(".slider-wrapper").css({
+            display: "grid"
         });
     });
-
-
-    $(".red-parts").mouseleave(function () {
-        $(this).css({
-            opacity: .1
-        });
-        $(".red-parts .path").css({
-            opacity: 1,
-            stroke: "#FFC0CB",
-            strokeWidth: "3"
-        });
-
-        anime({
-            targets: '.red-parts path',
-            strokeDashoffset: [anime.setDashoffset, 0],
-            easing: 'easeInOutSine',
-            duration: 800,
-            direction: 'alternate',
-            loop: false
-        });
-    });
-
-
+    //Outline the cow
 
     anime({
         targets: 'path',
@@ -63,24 +21,6 @@ $(document).ready(function () {
 
 
     //    RATINGS PAGE ***
-    $("div.cut").mouseover(function () {
-        anime({
-            targets: '.slider',
-            translateX: 100,
-            duration: 1000,
-            direction: 'alternate',
-            loop: false
-        });
-    });
-    $("div.cut").mouseleave(function () {
-        anime({
-            targets: '.slider',
-            translateX: 0,
-            duration: 1000,
-            direction: 'alternate',
-            loop: false
-        });
-    });
 
 
 
